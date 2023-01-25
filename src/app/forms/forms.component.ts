6e2e95583b6ddd7a6baf69b069becf5c080cb664
RavidAccion <ravid.rv@accionlabs.com>
2023-01-25 13:06:07 +0530

    25-01

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,55 +1,51 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
-  styleUrls: ['./forms.component.scss']
+  styleUrls: ['./forms.component.scss'],
 })
-
 export class FormsComponent implements OnInit {
-
-  myform: any = FormGroup;
-  paspat = " ^^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$. ";
-  constructor(private formBuilder: FormBuilder, private router: Router) { }
+  loginForm: any = FormGroup;
+  paspat = ' ^^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$. ';
+  constructor(private formBuilder: FormBuilder, private router: Router) {}
 
   ngOnInit(): void {
-    this.myformBuild();
+    this.loginFormBuild();
   }
 
-  myformBuild(): void {
-    this.myform = this.formBuilder.group({
-      email: new FormControl('', [
-        Validators.required,
-        Validators.email
-      ]
-      ),
+  loginFormBuild(): void {
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
       Password: new FormControl('', [
         Validators.required,
         Validators.minLength(5),
         // Validators.pattern(this.paspat)
-      ]
-      )
-    },)
+      ]),
+    });
   }
 
-
   subData() {
-    console.log(this.myform.value.email)
-    console.log(this.myform.value.Password)
+    console.log(this.loginForm.value.email);
+    console.log(this.loginForm.value.Password);
     // if (this.myform.value.email = "ravid@gmail.com" && this.myform.value.Password == 1234567) {
     //   console.log("ok")
     //   this.router.navigate([`/home`]);
     // } else {
     //   console.log("no")
     // }
-    if (this.myform.valid) {
-      console.log("ok")
+    if (this.loginForm.valid) {
+      console.log('ok');
       this.router.navigate([`/home`]);
     } else {
-      console.log("no")
+      console.log('no');
     }
   }
-
 }
--- src/app/home/Data.ts
+++
@@ -1,6 +0,0 @@
-export class Data {
-    constructor(
-      public id: number,
-      public name: string) { }
-  }
-  
